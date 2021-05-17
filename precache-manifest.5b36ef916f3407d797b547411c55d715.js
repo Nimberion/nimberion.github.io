70299bf28f21e7379faba1d508d4fda5ddb0b9bb
Nimberion <philipp.volkmer@web.de>
2021-05-17 16:27:48 +0200

    cc website

---
+++ precache-manifest.5b36ef916f3407d797b547411c55d715.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9f559224fc1bcc1767",
+    "url": "/css/app.f453c215.css"
+  },
+  {
+    "revision": "c4aec2f3498421f91d9f066ba8ed5626",
+    "url": "/img/de.c4aec2f3.svg"
+  },
+  {
+    "revision": "5aef6544adb270241539dad6b30b192d",
+    "url": "/img/en.5aef6544.svg"
+  },
+  {
+    "revision": "d2327021bdd3168469a2b8a761ead510",
+    "url": "/img/logo.d2327021.png"
+  },
+  {
+    "revision": "2e1ad91b1b349e8f64a3b0812e8fda6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df9f559224fc1bcc1767",
+    "url": "/js/app.8060be1f.js"
+  },
+  {
+    "revision": "2c74d1e6f713a638a4ff",
+    "url": "/js/chunk-vendors.dffd11cf.js"
+  },
+  {
+    "revision": "c5b441167eb1b1334b3aa3bc7b3b0554",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
